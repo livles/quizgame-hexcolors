47142e2182acddca661ee4b963b8a22d429a2bcc
livles <lucy.yang@outlook.de>
2025-09-05 13:37:46 +0200

    wrong stack added

--- game.js
+++ game.js
@@ -16,6 +16,8 @@ let hexcod = document.querySelector("#hex-code")
 let starttime = 0, duration = 0,averageduration = 0;
 let aside = document.querySelector("aside")
 let g = document.createElement('div')
+let wrong_answers = []
+const correct_answers = []
 g.className = 'tclose';
 
 
@@ -28,11 +30,6 @@ document.addEventListener("keydown",(e) =>   {
 
 // click
 submit.onclick = function () {
-    if (document.body.style.backgroundColor > "#333333") {
-        document.body.style.color = "white";
-    }else {
-        document.body.style.color = "black";
-    }
 
     if (starttime) {
         duration = performance.now() - starttime
@@ -45,56 +42,73 @@ submit.onclick = function () {
     answer.value = ""
     let color = document.body.style.backgroundColor
     
+
+    // evaluate last step
     if (index >= 0) {
-        if (index < 4 && colors.length && viewed_colors.length < 4) {
-        } else if (viewed_colors.length){
+        if (index < 4 && colors.length && (viewed_colors.length + wrong_answers.length) < 4) {
+            viewed_colors.push(color)
+            colors.splice(colors.indexOf(color),1)
+        } else if ( viewed_colors.length + wrong_answers.length){
             count ++;
             averageduration = ((count-1) * averageduration + duration) / (count)
-            console.log(color)
+
+            //remove color
+            if (viewed_colors.splice(viewed_colors.indexOf(color),1).length) {}
+            else {alert();wrong_answers.splice(wrong_answers.indexOf(color),1)}
+            
             if (text == color){
                 right_answers++;
+                correct_answers.push(color)
                 submitanswer.textContent = color + " was correct!" 
-                viewed_colors.splice(viewed_colors.indexOf(color),1)
                 if (right_answers == 148) {
                     alert("you have answered all colors correctly once.")
                 }
             } 
             else {
+                // alert()
+                if (wrong_answers.indexOf(color) == -1) {
+                    wrong_answers.push(color)
+                }
                 submitanswer.textContent = "Expected: " + color + ", but was: " + text + "."
             }
             footer.textContent = "correct answers: " + right_answers + " out of " + count + " with average answer time: " + (averageduration / 1000).toFixed(2)  + " s."
+            console.log(viewed_colors,correct_answers,wrong_answers)
         } 
     }
     
+    // show next card
+    index -= (index == 7 && viewed_colors.length) ? 1 : 0
     index = ( index + 1 ) % 8
     if (index < 4 && colors.length && viewed_colors.length < 4) {
         const rand_idx = Math.floor(Math.random() * colors.length)
         color  = colors[rand_idx];
-        viewed_colors.push(color)
-        colors.splice(rand_idx,1)
         document.body.style.backgroundColor = color;
         header.innerHTML = "Learn new HTML color: <br>" 
         inputlabel.textContent = "New color: " + document.body.style.backgroundColor
         submit.textContent = "Enter"
         answer.style.display = "none"
     }
-    else if (viewed_colors.length){
-        document.body.style.backgroundColor = viewed_colors[Math.floor(Math.random() * viewed_colors.length)];
+    else if (viewed_colors.length + wrong_answers.length){
+        // select randomly a color from viewed colors or wrong answered colors
+        let rand_idx = Math.floor(Math.random() * (viewed_colors.length + wrong_answers.length))
+        color = (rand_idx >= viewed_colors.length) ? wrong_answers[rand_idx - viewed_colors.length] : viewed_colors[rand_idx]
+        // console.log(viewed_colors,wrong_answers,correct_answers,rand_idx,color)
+        document.body.style.backgroundColor = color
+
         header.innerHTML = "Type in correct color: <br>"
         inputlabel.textContent = ""
         submit.textContent = "Enter"
-        answer.style.display = "inline-block"
+        answer.style.display = "flex" // flex?
     }
     let rgb_color = window.getComputedStyle(document.body).backgroundColor
-    const rbg_values = (rgb_color.split("(")[1].split(")")[0]).split(", ")
+    const rbg_values = rgb_color.split("(")[1].split(")")[0].split(", ")
     let r = parseInt(rbg_values[0])
     let g = parseInt(rbg_values[1])
     let b = parseInt(rbg_values[2])
+    bright = (r + g + b ) / 3 > 100
     const r_hex = r.toString(16).toUpperCase()
     const g_hex = g.toString(16).toUpperCase()
     const b_hex = b.toString(16).toUpperCase()
-    console.log(r_hex,g_hex,b_hex)
-    bright = (r + g + b ) / 3 > 150
     hexcod.textContent = "#" + r_hex + g_hex + b_hex
     document.body.style.color = bright ? "black" : "white"
     
